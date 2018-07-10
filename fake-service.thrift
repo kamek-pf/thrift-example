e74d0b6bb25b015f9d2795d0be531b93050da621
Kamek <b.kamek@gmail.com>
2018-07-10 18:06:51 -0400

    remove required on exception

--- fake-service.thrift
+++ fake-service.thrift
@@ -5,14 +5,14 @@ exception InvalidOperation {
 
 struct FakeProfile {
     1: required i32 id;
-    2: optional string first_name;
-    3: optional string last_name;
-    4: optional string email;
+    2: string first_name;
+    3: string last_name;
+    4: string email;
 }
 
 service FakeThingy {
     void ping(),
     i32 add(1: required i32 num1, 2: required i32 num2),
-    double divide(1: required double num1, 2: required double num2) throws (1: required InvalidOperation ouch),
+    double divide(1: required double num1, 2: required double num2) throws (1: InvalidOperation ouch),
     FakeProfile get_profile(1: required i32 id)
 }
